0d0854ff90eba6836b0cf99f43b413e1e04628e8
Gideon Osei <gideonosei022@gmail.com>
2025-11-15 02:50:13 +0000

    cloth recommendation based on weather condition

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,13 @@
+weather = input("What is today's weather? (sunny/rainy/cold): ")
+
+if weather == "sunny":
+    print("Wear a t-shirt and sunglasses")
+
+elif weather == "rainy":
+    print("raiDon't forget your umbrella and a raincoaty")
+
+elif weather == "cold":
+    print("Wear a warm coat and a scarf")
+
+else:
+    print("Sorry, I don't have recommendations for this weather")
