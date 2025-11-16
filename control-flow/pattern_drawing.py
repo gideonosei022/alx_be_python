0adae4060772214f4fa6b598b931b57767fa43b8
Gideon Osei <gideonosei022@gmail.com>
2025-11-16 11:29:30 +0000

    drawing pattern

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,12 @@
+
+size = int(input("Enter the size of the pattern: "))
+row = 0
+
+while row < size:
+    for i in range(size):
+        print("*", end="")
+    
+    print()
+    
+    # Increase the row count
+    row += 1
