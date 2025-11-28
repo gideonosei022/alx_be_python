04968fdab9cc62afd43f85e16cded4fb6355b74c
Gideon Osei <gideonosei022@gmail.com>
2025-11-28 14:29:34 +0000

    bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,27 @@
+class BankAccount:
+    def __init__(self, account_number, account_holder, initial_balance=0):
+        self.account_number = account_number
+        self.account_holder = account_holder
+        self.balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            return True
+        return False
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            return True
+        return False
+
+    def get_balance(self):
+        return self.balance
+
+    def get_account_info(self):
+        return {
+            "account_number": self.account_number,
+            "account_holder": self.account_holder,
+            "balance": self.balance
+        }
